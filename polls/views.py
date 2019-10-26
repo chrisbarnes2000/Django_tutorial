362c7ac35dfe6d9f250b0c50a81a57a5911442c0
Christopher Barnes <cbarnes7404@yahoo.com>
2019-10-25 23:37:25 -0700

    add detail template, 404 handling, and namespacing

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
+
 from .models import Question
 
 # Create your views here.
@@ -9,7 +11,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
