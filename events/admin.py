b18038d31aa880d9bc8f547e8110f5905ade469f
Christopher Barnes <cbarnes7404@yahoo.com>
2019-10-30 13:47:25 -0700

    add events model

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Question
-
+from .models import Event
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Event)
---
+++ events/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField('Person\'s Name', max_length=120)
+    DOB = models.DateTimeField('Date Of Birth')
+    phone = models.CharField('Phone number', max_length=16)
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    guest_list = []
+
+    def add_guest(self, name):
+        guest = models.ForeignKey(Person, on_delete=models.CASCADE)
+        self.guest_list.append(guest)
+
+    def __str__(self):
+        return self.name
+
+    #def get_by_id(self, _id):
+    #    self.objects.get(id= _id)
+
+
