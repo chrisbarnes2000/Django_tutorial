3bc34b204f4896c5574122baf908908edce6dba2
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-04 14:59:02 -0800

    moved style

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/polls"),
+    os.path.join(BASE_DIR, "static"),
 ]
