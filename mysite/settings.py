4d29dafe2807bbfbefa68b864c26eed0ae82b19c
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-04 13:54:15 -0800

    tweaked settings for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '!0lic&0^_vy8pif+68%2ekzezbc-1l_!$peil1!l^%je)=&33v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', 'django-tutorial-cb.herokuapp.com']
 
 # Application definition
 
@@ -121,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
